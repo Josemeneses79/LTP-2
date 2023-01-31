806c80cb811c0701a44ae61b15599532f2dc25a4
José Victor Meneses <82825346+Josemeneses79@users.noreply.github.com>
2023-01-31 09:06:26 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from classes import Roupas, Aula, Carne
+
+roupas1 = Roupas("Lacorte", "Jaqueta")
+
+print(roupas1.description())
+
+aulas1 = Aula("LTP-2", "labin-2")
+
+print(aulas1.description())
+
+carne1 = Carne("Picanha", 49.99)
+
+print(carne1.description())
+              
